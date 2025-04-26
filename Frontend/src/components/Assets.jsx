d2921b28170e07490e1b2f2edfe21ed60dac77d8
T Manikiran <165704175+manikiran231@users.noreply.github.com>
2025-04-26 16:29:28 +0530

    Update Assets.jsx

--- Frontend/src/components/Assets.jsx
+++ Frontend/src/components/Assets.jsx
@@ -1,17 +1,17 @@
-import workout from "../assets/workout.png";
-import recipes from "../assets/recipes.png";
-import languageLearning from "../assets/language-learning.jpg";
-import mental from "../assets/mental.png";
-import virtualTrips from "../assets/virtual-trips.jpg";
-import musicArts from "../assets/music-arts.jpg";
-import community from "../assets/community.png";
-import codingForKids from "../assets/coding-for-kids.jpg";
-import React from 'react'
+// import workout from "../assets/workout.png";
+// import recipes from "../assets/recipes.png";
+// import languageLearning from "../assets/language-learning.jpg";
+// import mental from "../assets/mental.png";
+// import virtualTrips from "../assets/virtual-trips.jpg";
+// import musicArts from "../assets/music-arts.jpg";
+// import community from "../assets/community.png";
+// import codingForKids from "../assets/coding-for-kids.jpg";
+// import React from 'react'
 
-function Assets() {
-  return (
-    <div>Assets</div>
-  )
-}
+// function Assets() {
+//   return (
+//     <div>Assets</div>
+//   )
+// }
 
-export default Assets
+// export default Assets
